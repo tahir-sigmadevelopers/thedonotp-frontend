ff459a044e56f8de283b039be22e15ed42ffba8d
Mohammad Tahir <starghareeb@gmail.com>
2025-06-11 09:12:37 +0500

    new api credentials added

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const API_URL = 'https://otpbackend-eta.vercel.app/api';
+// const API_URL = 'http://localhost:5000/api';
 
 // Create axios instance
 const api = axios.create({
